7dfcac56e3066ffa5e351fe529c5d963c63037c9
PedroMakengo <pedromakengomiguel@gmail.com>
2024-04-20 22:36:02 +0100

    Utilizado as regras de estilização

--- App.tsx
+++ App.tsx
@@ -1,22 +1,30 @@
-import { Fragment } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: '#131016', padding: 24 }}>
-      <Text
-        style={{
-          color: '#fdfcfe',
-          fontSize: 24,
-          fontWeight: 'bold',
-          marginTop: 48,
-        }}
-      >
-        Nome do evento
-      </Text>
-      <Text style={{ color: '#6b6b6b', fontSize: 16 }}>
-        Sexta, 16 de Abril de 2024
-      </Text>
+    <View style={style.container}>
+      <Text style={style.eventName}>Nome do evento</Text>
+      <Text style={style.eventDate}>Sexta, 16 de Abril de 2024</Text>
     </View>
   )
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24,
+  },
+
+  eventName: {
+    color: '#fdfcfe',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+
+  eventDate: {
+    color: '#6b6b6b',
+    fontSize: 16,
+  },
+})
